ef64f4855ca20072899542fbb01d77fd78e3266f
junbig <kjgi73k@gmail.com>
2023-03-03 19:05:47 +0900

    feat: Swagger 설정 변경
    - 도메인별 그룹화 설정 -> 도메인별 개발시 추가바람.
    - Authentication 설정할 수 있도록 설정

--- src/main/java/com/sluv/server/global/config/SwaggerConfig.java
+++ src/main/java/com/sluv/server/global/config/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package com.sluv.server.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,9 +20,46 @@ public class SwaggerConfig {
                 .version("v1.0.0")
                 .title("Sluv")
                 .description("Sluv Swagger");
+        // SecuritySecheme명
+        String jwtSchemeName = "AccessToken";
+        // API 요청헤더에 인증정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("bearer")
+                        .bearerFormat("JWT")); // 토큰 형식을 지정하는 임의의 문자(Optional
 
         return new OpenAPI()
-                .info(info);
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+
+    @Bean
+    public GroupedOpenApi allGroup(){
+        return GroupedOpenApi.builder()
+                .group("All")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi authGroup(){
+        return GroupedOpenApi.builder()
+                .group("Auth")
+                .pathsToMatch("/auth/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi userGroup(){
+        return GroupedOpenApi.builder()
+                .group("User")
+                .pathsToMatch("/user/**")
+                .build();
     }
 
 }

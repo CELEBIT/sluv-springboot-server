3b7f4d004f6e2f97036624cb27a90d4417e9747f
junbig <kjgi73k@gmail.com>
2023-03-14 10:57:22 +0900

    finish

--- src/main/java/com/sluv/server/domain/question/entity/QuestionBuy.java
+++ src/main/java/com/sluv/server/domain/question/entity/QuestionBuy.java
@@ -3,6 +3,7 @@ package com.sluv.server.domain.question.entity;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
@@ -10,11 +11,17 @@ import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
-@SuperBuilder
+//@SuperBuilder
 @DiscriminatorValue("Buy")
 public class QuestionBuy extends Question{
 
 
     private LocalDateTime voteEndTime;
+
+    @Builder
+    public QuestionBuy(Long id, Long userId, String title, String content, Long searchNum, LocalDateTime voteEndTime) {
+        super(id, userId, title, content, searchNum);
+        this.voteEndTime = voteEndTime;
+    }
 
 }

fe1f5d99f0ff8a803e9a1c22bf9adc8fdd35a262
junbig <kjgi73k@gmail.com>
2023-03-14 13:00:05 +0900

    [#18] feat : ItemLink with Item
    - ItemLink
        - Long itemId -> Item item
            - ManyToOne join item_item_id
    - Item
        - List<ItemLink> itemLinkListk

--- src/main/java/com/sluv/server/domain/item/entity/Item.java
+++ src/main/java/com/sluv/server/domain/item/entity/Item.java
@@ -92,6 +92,9 @@ public class Item extends BaseEntity {
     @OneToMany(mappedBy = "item")
     List<ItemLike> itemLikeList;
 
+    @OneToMany(mappedBy = "item")
+    List<ItemLink> itemLinkList;
+
     @Builder
     public Item(Long id, User user, Celeb celeb, NewCeleb newCeleb, ItemCategory category, Brand brand, NewBrand newBrand, String name, LocalDateTime whenDiscovery, String whereDiscovery, int price, String color, String additionalInfo, String infoSource, ItemStatus itemStatus) {
         this.id = id;

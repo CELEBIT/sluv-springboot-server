6b4cf83596c6b794195bd877633b9de9c0c85621
junbig <kjgi73k@gmail.com>
2023-03-14 14:12:37 +0900

    [#18] feat : QuestionRecommendCategory
    - QuestionRecommendCategory
        - Long questionId -> Question question
            - ManyToOne join question_question_id

--- src/main/java/com/sluv/server/domain/question/entity/QuestionRecommend.java
+++ src/main/java/com/sluv/server/domain/question/entity/QuestionRecommend.java
@@ -1,10 +1,19 @@
 package com.sluv.server.domain.question.entity;
 
+import com.sluv.server.domain.user.entity.User;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor
 @DiscriminatorValue("Recommend")
 public class QuestionRecommend extends Question{
-
+    @Builder
+    public QuestionRecommend(Long id, User user, String title, String content, Long searchNum) {
+        super(id, user, title, content, searchNum);
+    }
 }

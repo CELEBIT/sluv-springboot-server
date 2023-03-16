eeddb7b62a57f8d669451dc76a5cf2ecb1613226
junbig <kjgi73k@gmail.com>
2023-03-16 19:14:40 +0900

    [#22] 관심셀럽 검색 API
    - UserController 수정.
    - UserService 수정
    - InterestedCelebRepository 생성
    
    - CelebResDto 생성(그룹 및 그룹이 없는 셀럽)
    - MemeberResDto 생성(그룹이 있는 셀럽)

--- src/main/java/com/sluv/server/domain/user/controller/UserController.java
+++ src/main/java/com/sluv/server/domain/user/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.sluv.server.domain.user.controller;
 
 
+import com.sluv.server.domain.celeb.entity.Celeb;
+import com.sluv.server.domain.user.entity.User;
 import com.sluv.server.domain.user.service.UserService;
 import com.sluv.server.global.common.response.SuccessDataResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,13 +21,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
 
-    @GetMapping("")
-    public ResponseEntity<?> getUserId(HttpServletRequest request) {
+    @GetMapping("/celeb")
+    public ResponseEntity<SuccessDataResponse<List<Celeb>>> getInterestedCeleb(@AuthenticationPrincipal User user){
 
-            return ResponseEntity.ok().body(SuccessDataResponse.builder()
-                                                            .result(userService.getUserIdByToken(request))
-                                                            .build()
-                            );
+
+        return ResponseEntity.ok().body(SuccessDataResponse.<List<Celeb>>builder()
+                                                            .result(userService.findUserInterestedCeleb(user))
+                                                            .build());
     }
+
 
 }
--- src/main/java/com/sluv/server/domain/user/service/UserService.java
+++ src/main/java/com/sluv/server/domain/user/service/UserService.java
@@ -1,5 +1,9 @@
 package com.sluv.server.domain.user.service;
 
+import com.sluv.server.domain.celeb.dto.CelebResDto;
+import com.sluv.server.domain.celeb.entity.Celeb;
+import com.sluv.server.domain.celeb.entity.InterestedCeleb;
+import com.sluv.server.domain.celeb.repository.InterestedCelebRepository;
 import com.sluv.server.domain.user.entity.User;
 import com.sluv.server.domain.user.dto.UserDto;
 import com.sluv.server.domain.user.repository.UserRepository;
@@ -8,11 +12,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final InterestedCelebRepository interestedCelebRepository;
     private final JwtProvider jwtProvider;
 
     public UserDto getUserIdByToken(HttpServletRequest request) {
@@ -20,5 +29,14 @@ public class UserService {
             return UserDto.builder()
                             .id(jwtProvider.getUserId(token))
                             .build();
+    }
+
+    /**
+     * == user의 관심 Celeb 검색
+     * @param user
+     */
+    public List<Celeb> findUserInterestedCeleb(User user) {
+
+        return null;
     }
 }

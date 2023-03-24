6bcd5a3923dc061b772e89edd66c01f8764ec0df
junbig <kjgi73k@gmail.com>
2023-03-24 18:58:08 +0900

    [#37] feat : HashtagRepositoryCustom 설정
    - HashtagRepositoryCustom.java 생성
    - HashtagRepositoryImpl.java 생성

---
+++ src/main/java/com/sluv/server/domain/item/repository/impl/HashtagRepositoryCustom.java
@@ -0,0 +1,9 @@
+package com.sluv.server.domain.item.repository.impl;
+
+import com.querydsl.core.Tuple;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface HashtagRepositoryCustom {
+    Page<Tuple> findAllByContent(String name, Pageable pageable);
+}

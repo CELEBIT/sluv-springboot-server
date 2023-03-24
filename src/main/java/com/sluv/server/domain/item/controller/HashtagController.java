0643b90bb52aa120fe88224d9bd731bc357b4777
junbig <kjgi73k@gmail.com>
2023-03-24 19:23:53 +0900

    [#37] feat : Swagger 어노테이션 추가
    - HashtagController에 Swagger 어노테이션 추가
    - HashtagResponseDto에 Swagger 어노테이션 추가
    - SwaggerConfig에 Item 그룹 추가

--- src/main/java/com/sluv/server/domain/item/controller/HashtagController.java
+++ src/main/java/com/sluv/server/domain/item/controller/HashtagController.java
@@ -2,7 +2,13 @@ package com.sluv.server.domain.item.controller;
 
 import com.sluv.server.domain.item.dto.HashtagResponseDto;
 import com.sluv.server.domain.item.service.HashtagService;
+import com.sluv.server.global.common.response.ErrorResponse;
 import com.sluv.server.global.common.response.SuccessDataResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +26,15 @@ public class HashtagController {
 
     private final HashtagService hashtagService;
 
+    @Operation(
+            summary = "해쉬태그 검색(Pagination)",
+            description = "Item 등록 시 추가 정보에 해쉬태그 등록 시 사용"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "1000", description = "요청성공"),
+            @ApiResponse(responseCode = "5000", description = "서버내부 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "5001", description = "DB 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @GetMapping("")
     public ResponseEntity<SuccessDataResponse<List<HashtagResponseDto>>> getHashtag(@RequestParam String name, Pageable pageable){
 
--- src/main/java/com/sluv/server/domain/item/dto/HashtagResponseDto.java
+++ src/main/java/com/sluv/server/domain/item/dto/HashtagResponseDto.java
@@ -1,6 +1,7 @@
 package com.sluv.server.domain.item.dto;
 
 import com.sluv.server.domain.item.entity.hashtag.Hashtag;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
@@ -10,8 +11,11 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class HashtagResponseDto {
+    @Schema(description = "해쉬태그 id")
     private Long hashtagId;
+    @Schema(description = "해쉬태그 내용")
     private String hashtagContent;
+    @Schema(description = "해쉬태그 누적수")
     private Long count;
 
     @Builder

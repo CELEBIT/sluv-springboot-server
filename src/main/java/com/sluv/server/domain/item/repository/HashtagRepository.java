8e157f519763ef95d50fa12aea209188f6ab3701
junbig <kjgi73k@gmail.com>
2023-03-25 16:54:28 +0900

    [#39] feat : findByContent 추가
    - Content이름으로 Hashtag를 찾는 findByContent 생성
    - null 가능하게 리턴값을 Optional로 설정.

--- src/main/java/com/sluv/server/domain/item/repository/HashtagRepository.java
+++ src/main/java/com/sluv/server/domain/item/repository/HashtagRepository.java
@@ -4,5 +4,9 @@ import com.sluv.server.domain.item.entity.hashtag.Hashtag;
 import com.sluv.server.domain.item.repository.impl.HashtagRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface HashtagRepository extends JpaRepository<Hashtag, Long>, HashtagRepositoryCustom {
+
+    Optional<Hashtag> findByContent(String hashtagContent);
 }

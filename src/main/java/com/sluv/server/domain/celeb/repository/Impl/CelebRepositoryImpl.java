28a115de053808b9f1ef8613f3bf8113de7b72af
junbig <kjgi73k@gmail.com>
2023-03-29 00:41:18 +0900

    [#22] feat: CelebRepository 생성
    - 관심 샐럽을 검색하기 위한 Repository 생성
    - CelebRepository.java 생성
    - CelebRepositoryCustom.java 생성
    - CelebRepositoryImpl.javav 생성
        - interested_celeb 테이블에서 user_id와 일치하는 row의 celeb 조회

--- src/main/java/com/sluv/server/domain/celeb/repository/Impl/CelebRepositoryImpl.java
+++ src/main/java/com/sluv/server/domain/celeb/repository/Impl/CelebRepositoryImpl.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
+import static com.sluv.server.domain.user.entity.QUser.user;
 import static com.sluv.server.domain.celeb.entity.QCeleb.celeb;
 import static com.sluv.server.domain.celeb.entity.QInterestedCeleb.interestedCeleb;
 
@@ -16,11 +17,11 @@ public class CelebRepositoryImpl implements CelebRepositoryCustom{
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public List<Celeb> findInterestedCeleb(User user) {
+    public List<Celeb> findInterestedCeleb(User _user) {
 
-        return jpaQueryFactory.select(celeb)
+        return jpaQueryFactory.select(interestedCeleb.celeb)
                 .from(interestedCeleb)
-                .where(interestedCeleb.user.eq(user))
+                .where(interestedCeleb.user.eq(_user))
                 .fetch();
     }
 }

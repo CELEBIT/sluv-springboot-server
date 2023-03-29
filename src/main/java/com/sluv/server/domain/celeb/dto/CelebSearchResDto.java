dbd4346d88d4f9a80b6c60323490052f8b1a7f6a
junbig <kjgi73k@gmail.com>
2023-03-29 23:24:25 +0900

    [#44] feat : Swagger 어노테이션 추가
    - CelebController에 Swagger 어노테이션 추가
    - CelebSearchResDto에 Swagger 어노테이션 추가

--- src/main/java/com/sluv/server/domain/celeb/dto/CelebSearchResDto.java
+++ src/main/java/com/sluv/server/domain/celeb/dto/CelebSearchResDto.java
@@ -1,6 +1,7 @@
 package com.sluv.server.domain.celeb.dto;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
@@ -11,8 +12,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @JsonPropertyOrder({"id", "CelebNameKr", "CelebNameEn"})
 public class CelebSearchResDto {
+    @Schema(description = "Celeb id")
     private Long id;
+    @Schema(description = "Celeb 한글 이름")
     private String CelebNameKr;
+    @Schema(description = "Celeb 영어 이름")
     private String CelebNameEn;
 
     @Builder

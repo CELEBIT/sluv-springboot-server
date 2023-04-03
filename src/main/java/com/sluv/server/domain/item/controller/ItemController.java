a9714aaa1dec43990f78bfca8db9d1aec1a03433
junbig <kjgi73k@gmail.com>
2023-04-03 13:42:29 +0900

    [#50] feat: Swagger 어노테이션 추가
    - ItemController에 Swagger 어노테이션 추가
    - ItemPostReqDto에 Swagger 어노테이션 추가

--- src/main/java/com/sluv/server/domain/item/controller/ItemController.java
+++ src/main/java/com/sluv/server/domain/item/controller/ItemController.java
@@ -3,7 +3,13 @@ package com.sluv.server.domain.item.controller;
 import com.sluv.server.domain.item.dto.ItemPostReqDto;
 import com.sluv.server.domain.item.service.ItemService;
 import com.sluv.server.domain.user.entity.User;
+import com.sluv.server.global.common.response.ErrorResponse;
 import com.sluv.server.global.common.response.SuccessResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -16,6 +22,15 @@ public class ItemController {
 
     private final ItemService itemService;
 
+    @Operation(
+            summary = "아이템 등록",
+            description = "아이템 등록 요청"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "1000", description = "요청성공"),
+            @ApiResponse(responseCode = "5000", description = "서버내부 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "5001", description = "DB 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @PostMapping("")
     public ResponseEntity<SuccessResponse> postItem(@AuthenticationPrincipal User user, @RequestBody ItemPostReqDto reqDto){
 
--- src/main/java/com/sluv/server/domain/item/dto/ItemPostReqDto.java
+++ src/main/java/com/sluv/server/domain/item/dto/ItemPostReqDto.java
@@ -1,5 +1,6 @@
 package com.sluv.server.domain.item.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,17 +16,28 @@ public class ItemPostReqDto {
      * 날짜, 장소, 추가 정보, 구매 링크
      */
 
+    @Schema(description = "item 이미지 리스트 \"{대표여부 0 or 1}\":\"이미지링크\"")
     private List<Map<Long, String>> imgList;
+    @Schema(description = "celebId")
     private Long celebId;
+    @Schema(description = "발견 시간 ex)2021-11-20T09:10:20")
     private LocalDateTime whenDiscovery;
+    @Schema(description = "발견 장소")
     private String whereDiscovery;
+    @Schema(description = "아이템 카테고리 Id")
     private Long categoryId;
+    @Schema(description = "브랜드 Id")
     private Long brandId;
+    @Schema(description = "아이템명")
     private String itemName;
+    @Schema(description = "가격")
     private Integer price;
 
+    @Schema(description = "추가정보")
     private String additionalInfo;
+    @Schema(description = "해쉬태그 Id 리스트")
     private List<Long> hashTagIdList;
+    @Schema(description = "item 이미지 리스트 \"제목\":\"링크\"")
     private List<Map<String, String>> infoSourceList;
 
 }

dbbb3deb307d1a8d41233e452397ae08cced5acf
junbig <kjgi73k@gmail.com>
2023-04-09 20:49:16 +0900

    [#54] feat: 인기장소 관련 Swagger 어노테이션 추가
    - ItemController에 Swagger 어노테이션 추가
    - PlaceRankResDto에 Swagger 어노테이션 추가

--- src/main/java/com/sluv/server/domain/item/dto/PlaceRankResDto.java
+++ src/main/java/com/sluv/server/domain/item/dto/PlaceRankResDto.java
@@ -1,5 +1,6 @@
 package com.sluv.server.domain.item.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,5 +11,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class PlaceRankResDto {
+    @Schema(description = "장소명")
     private String placeName;
 }

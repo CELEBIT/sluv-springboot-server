2480a58674cc4f732ed38596df96ed9f72cd06e1
junbig <kjgi73k@gmail.com>
2023-04-09 20:43:25 +0900

    [#54] feat: PlaceRankRepository 관련 추가 및 수정
    - PlaceRankRepositoryCustom.java 생성
    - PlaceRankRepositoryImpl.java 생성
    
    - PlaceRankRepositoryImpl에 findTopPlace 구현.

--- src/main/java/com/sluv/server/domain/item/repository/PlaceRankRepository.java
+++ src/main/java/com/sluv/server/domain/item/repository/PlaceRankRepository.java
@@ -1,7 +1,8 @@
 package com.sluv.server.domain.item.repository;
 
 import com.sluv.server.domain.item.entity.PlaceRank;
+import com.sluv.server.domain.item.repository.impl.PlaceRankRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PlaceRankRepository extends JpaRepository<PlaceRank, Long> {
+public interface PlaceRankRepository extends JpaRepository<PlaceRank, Long>, PlaceRankRepositoryCustom {
 }

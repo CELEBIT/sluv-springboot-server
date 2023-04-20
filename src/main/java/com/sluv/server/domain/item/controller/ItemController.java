05f3aaf451cf8a43ac42bf3beb6b05f3aa3a534c
KJBig <kjgi73k@gmail.com>
2023-04-20 18:48:37 +0900

    [#64] feat : Swagger 어노테이션 추가
    - ItemController에 Swagger 어노테이션 추가

--- src/main/java/com/sluv/server/domain/item/controller/ItemController.java
+++ src/main/java/com/sluv/server/domain/item/controller/ItemController.java
@@ -136,6 +136,15 @@ public class ItemController {
         return ResponseEntity.ok().body(new SuccessResponse());
     }
 
+    @Operation(
+            summary = "임시저장 아이템 전체삭제",
+            description = "사용자의 임시저장 아이템 전체삭제."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "1000", description = "요청성공"),
+            @ApiResponse(responseCode = "5000", description = "서버내부 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "5001", description = "DB 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @DeleteMapping("/temp")
     public ResponseEntity<SuccessResponse> deleteAllTempItem(@AuthenticationPrincipal User user){
         tempItemService.deleteAllTempItem(user);

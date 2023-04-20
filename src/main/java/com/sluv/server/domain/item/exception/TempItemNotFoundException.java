bd18b3f06bfea265843868db2c1f14fc7718efee
KJBig <kjgi73k@gmail.com>
2023-04-20 16:08:12 +0900

    [#60] feat: TempItemNotFoundException 생성
    - TempItem을 찾지 못했을 때 발생하는 Exception 생성

---
+++ src/main/java/com/sluv/server/domain/item/exception/TempItemNotFoundException.java
@@ -0,0 +1,13 @@
+package com.sluv.server.domain.item.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class TempItemNotFoundException extends ItemException {
+    private static final int ERROR_CODE = 2008;
+    private static final String MESSAGE = "존재하지 Temp Item입니다.";
+    private static final HttpStatus STATUS = HttpStatus.BAD_REQUEST;
+
+    public TempItemNotFoundException() {
+        super(ERROR_CODE, STATUS, MESSAGE);
+    }
+}

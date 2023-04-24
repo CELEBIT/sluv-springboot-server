847e681ef81be9969a28a4797f90601defb38a77
junker <kjgi73k@gmail.com>
2023-04-24 19:00:17 +0900

    [#70] feat : cors 설정 허용
    - cors().disable() -> cors()로 변경

--- src/main/java/com/sluv/server/global/config/security/SpringSecurityConfig.java
+++ src/main/java/com/sluv/server/global/config/security/SpringSecurityConfig.java
@@ -13,6 +13,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 
 @Configuration
@@ -56,7 +61,8 @@ public class SpringSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
                 .csrf().disable()
-                .cors().disable()
+                .cors()
+                .and()
                 // stateless
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)

6af9a9dfa328cede3dcc2220d85c683130b5e82a
junker <kjgi73k@gmail.com>
2023-05-06 16:37:36 +0900

    Header 추가시 Cors 에러 발생 수정
    - Authorization Header 추가 시 Cors 에러 방지

--- src/main/java/com/sluv/server/global/config/security/CorsConfig.java
+++ src/main/java/com/sluv/server/global/config/security/CorsConfig.java
@@ -16,6 +16,7 @@ public class CorsConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);

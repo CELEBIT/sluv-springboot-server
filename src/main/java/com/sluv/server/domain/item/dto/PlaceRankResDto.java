61f4f0cfb93d410b701eb6ece53f56429e4cd600
junker <kjgi73k@gmail.com>
2023-05-08 17:21:55 +0900

    feat : 유저 최근 입력 장소 검색 기능 추가
    - 필요한 파일 생성

---
+++ src/main/java/com/sluv/server/domain/item/dto/PlaceRankResDto.java
@@ -0,0 +1,16 @@
+package com.sluv.server.domain.item.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PlaceRankResDto {
+    @Schema(description = "입력한 장소 이름")
+    private String placeName;
+}

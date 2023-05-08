a538cc1deb4a32ae2893356bd652579f44854921
junker <kjgi73k@gmail.com>
2023-05-08 19:38:55 +0900

    [#96] feat : ItemEditReq 관련 Repository 생성
    - ItemEditReqRepository 생성

---
+++ src/main/java/com/sluv/server/domain/item/repository/ItemEditReqRepository.java
@@ -0,0 +1,7 @@
+package com.sluv.server.domain.item.repository;
+
+import com.sluv.server.domain.item.entity.ItemEditReq;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemEditReqRepository extends JpaRepository<ItemEditReq, Long> {
+}

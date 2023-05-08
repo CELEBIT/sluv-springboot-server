c8627c5e5e841b55909655c14f4fb48807f4ab2a
junker <kjgi73k@gmail.com>
2023-05-08 20:13:22 +0900

    [#98] feat : 아이템 게시글 신고 로직 추가
    - ItemReportReqDto 생성
    - ItemReportRepository 생성
    - ItemReportService 생성

---
+++ src/main/java/com/sluv/server/domain/item/repository/ItemReportRepository.java
@@ -0,0 +1,8 @@
+package com.sluv.server.domain.item.repository;
+
+import com.sluv.server.domain.item.entity.ItemReport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemReportRepository extends JpaRepository<ItemReport, Long> {
+
+}

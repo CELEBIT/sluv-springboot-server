0faa173d508258b59a55f2c7893d1b43a7fd04dc
junker <kjgi73k@gmail.com>
2023-05-08 17:18:57 +0900

    feat : 최근 입력한 장소 검색 기능
    - 최근 입력한 장소 중 가장 최근 20개를 검색

--- src/main/java/com/sluv/server/domain/item/repository/PlaceRankRepository.java
+++ src/main/java/com/sluv/server/domain/item/repository/PlaceRankRepository.java
@@ -1,9 +1,13 @@
 package com.sluv.server.domain.item.repository;
 
 import com.sluv.server.domain.item.entity.PlaceRank;
+import com.sluv.server.domain.item.repository.impl.PlaceRankRepositoryCustom;
+import com.sluv.server.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PlaceRankRepository extends JpaRepository<PlaceRank, Long> {
+import java.util.Arrays;
+
+public interface PlaceRankRepository extends JpaRepository<PlaceRank, Long>, PlaceRankRepositoryCustom {
     void deleteAllByUserId(Long id);
 
     void deleteByUserIdAndPlace(Long id, String placeName);

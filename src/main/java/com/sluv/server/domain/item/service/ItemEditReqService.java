29d92ad2e4deb8a9d63129897146156fc277604c
junker <kjgi73k@gmail.com>
2023-05-08 19:41:57 +0900

    [#96] feat : Item 게시글 정보 수정 요청 기능 추가
    - ItemEditReqService에 비즈니스 로직 추가
    - UserController에 API추가

---
+++ src/main/java/com/sluv/server/domain/item/service/ItemEditReqService.java
@@ -0,0 +1,31 @@
+package com.sluv.server.domain.item.service;
+
+import com.sluv.server.domain.item.dto.ItemEditReqDto;
+import com.sluv.server.domain.item.entity.Item;
+import com.sluv.server.domain.item.entity.ItemEditReq;
+import com.sluv.server.domain.item.exception.ItemNotFoundException;
+import com.sluv.server.domain.item.repository.ItemEditReqRepository;
+import com.sluv.server.domain.item.repository.ItemRepository;
+import com.sluv.server.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ItemEditReqService {
+    private final ItemEditReqRepository itemEditReqRepository;
+    private final ItemRepository itemRepository;
+
+    public void postItemEdit(User user, Long itemId, ItemEditReqDto dto){
+        Item item = itemRepository.findById(itemId).orElseThrow(ItemNotFoundException::new);
+
+        itemEditReqRepository.save(
+                ItemEditReq.builder()
+                            .requester(user)
+                            .item(item)
+                            .itemEditReqReason(dto.getEditReqReason())
+                            .content(dto.getContent())
+                            .build()
+        );
+    }
+}
--- src/main/java/com/sluv/server/domain/user/controller/UserController.java
+++ src/main/java/com/sluv/server/domain/user/controller/UserController.java
@@ -2,6 +2,8 @@ package com.sluv.server.domain.user.controller;
 
 
 import com.sluv.server.domain.celeb.dto.InterestedCelebParentResDto;
+import com.sluv.server.domain.item.dto.ItemEditReqDto;
+import com.sluv.server.domain.item.service.ItemEditReqService;
 import com.sluv.server.domain.user.entity.User;
 import com.sluv.server.domain.user.service.UserService;
 import com.sluv.server.global.common.response.ErrorResponse;
@@ -24,6 +26,7 @@ import java.util.List;
 @RequestMapping("/app/user")
 public class UserController {
     private final UserService userService;
+    private final ItemEditReqService itemEditReqService;
     @Operation(
             summary = "*유저의 관심 샐럽 조회",
             description = "유저를 기준으로 InterstedCeleb 테이블에서 일치하는 Celeb을 검색"
@@ -53,6 +56,23 @@ public class UserController {
     @PostMapping("/{userId}/follow")
     public ResponseEntity<SuccessResponse> postUserFollow(@AuthenticationPrincipal User user, @PathVariable(name = "userId") Long userId) {
         userService.postUserFollow(user, userId);
+        return ResponseEntity.ok().body(
+                new SuccessResponse()
+        );
+    }
+    @Operation(
+            summary = "*아이템 게시글 수정 요청",
+            description = "유저가 특정 아이템 게시글의 내용을 수정 요청" +
+                    " (User Id 필요)"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "1000", description = "요청성공"),
+            @ApiResponse(responseCode = "5000", description = "서버내부 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "5001", description = "DB 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @PostMapping("/{itemId}/edit-req")
+    public ResponseEntity<SuccessResponse> postItemEdit(@AuthenticationPrincipal User user, @PathVariable(name = "itemId") Long itemId, @RequestBody ItemEditReqDto dto) {
+        itemEditReqService.postItemEdit(user, itemId, dto);
         return ResponseEntity.ok().body(
                 new SuccessResponse()
         );

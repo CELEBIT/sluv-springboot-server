8f771cf3d56741a3669399f55035e1310b617c4c
junker <kjgi73k@gmail.com>
2023-05-08 18:02:57 +0900

    [#94] feat : follow 기능 추가
    - 유저간의 follow 기능 추가

--- src/main/java/com/sluv/server/domain/user/controller/UserController.java
+++ src/main/java/com/sluv/server/domain/user/controller/UserController.java
@@ -6,6 +6,7 @@ import com.sluv.server.domain.user.entity.User;
 import com.sluv.server.domain.user.service.UserService;
 import com.sluv.server.global.common.response.ErrorResponse;
 import com.sluv.server.global.common.response.SuccessDataResponse;
+import com.sluv.server.global.common.response.SuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -14,9 +15,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -42,5 +41,21 @@ public class UserController {
                                                             .build());
     }
 
+    @Operation(
+            summary = "*유저의 관심 샐럽 조회",
+            description = "유저를 기준으로 InterstedCeleb 테이블에서 일치하는 Celeb을 검색"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "1000", description = "요청성공"),
+            @ApiResponse(responseCode = "5000", description = "서버내부 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "5001", description = "DB 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @PostMapping("/{userId}/follow")
+    public ResponseEntity<SuccessResponse> postUserFollow(@AuthenticationPrincipal User user, @PathVariable(name = "userId") Long userId) {
+        userService.postUserFollow(user, userId);
+        return ResponseEntity.ok().body(
+                new SuccessResponse()
+        );
+    }
 
 }

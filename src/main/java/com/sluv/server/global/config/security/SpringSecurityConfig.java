f34b6409748275960ea177757bb909776f7e8ff8
junker <kjgi73k@gmail.com>
2023-05-12 21:44:15 +0900

    HttpMethod.OPTIONS all permitAll

--- src/main/java/com/sluv/server/global/config/security/SpringSecurityConfig.java
+++ src/main/java/com/sluv/server/global/config/security/SpringSecurityConfig.java
@@ -66,6 +66,7 @@ public class SpringSecurityConfig {
                 .httpBasic().disable()
 
                 .authorizeHttpRequests((request) -> request // 허용 범위 설정
+                                .requestMatchers(HttpMethod.OPTIONS, "/**/*").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/app/item/category").permitAll()
                                 .requestMatchers(AUTH_URL).authenticated() // 허용범위
                         .anyRequest().permitAll()

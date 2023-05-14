c5d2547eee08d0d88c5af7263335597ea34c6d07
junker <kjgi73k@gmail.com>
2023-05-15 04:40:23 +0900

    [#54] feat : S3 기본설정
    - S3Config 생성
    - PreSingedUrlResDto 생성
    - ImgExtension 생성

---
+++ src/main/java/com/sluv/server/global/s3/PreSingedUrlResDto.java
@@ -0,0 +1,15 @@
+package com.sluv.server.global.s3;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PreSingedUrlResDto {
+    private String preSignedUrl;
+    private String key;
+}

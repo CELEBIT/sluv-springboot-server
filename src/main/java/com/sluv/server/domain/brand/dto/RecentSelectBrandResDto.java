4e3b5462d0281a0a43ecfb5d98c4b2b4722b799f
junker <kjgi73k@gmail.com>
2023-05-15 13:51:04 +0900

    최근 검색 브랜드 Dto 수정
    - RecentSelectBrandResDto에 brandImgUrl 추가

--- src/main/java/com/sluv/server/domain/brand/dto/RecentSelectBrandResDto.java
+++ src/main/java/com/sluv/server/domain/brand/dto/RecentSelectBrandResDto.java
@@ -12,13 +12,16 @@ public class RecentSelectBrandResDto {
     private Long id;
     @Schema(description = "브랜드 이름")
     private String brandName;
+    @Schema(description = "브랜드 Img Url")
+    private String brandImgUrl;
     @Schema(description = "브랜드(Y)와 뉴브랜드(N)를 구분하는 플래그")
     private String flag;
 
     @Builder
-    public RecentSelectBrandResDto(Long id, String brandName, String flag) {
+    public RecentSelectBrandResDto(Long id, String brandName, String brandImgUrl, String flag) {
         this.id = id;
         this.brandName = brandName;
+        this.brandImgUrl = brandImgUrl;
         this.flag = flag;
     }
 }

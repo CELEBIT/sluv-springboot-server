80a9a4edf173f2adb64f255c7d95bd6fcbcef6ce
junker <kjgi73k@gmail.com>
2023-05-16 14:46:32 +0900

    [#119] feat : QuestionRepository 생성
    - QuestionImgRepository 생성
    - QuestionItemRepository 생성
    - QuestionRepository 생성

---
+++ src/main/java/com/sluv/server/domain/question/repository/QuestionItemRepository.java
@@ -0,0 +1,7 @@
+package com.sluv.server.domain.question.repository;
+
+import com.sluv.server.domain.question.entity.QuestionItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionItemRepository extends JpaRepository<QuestionItem, Long> {
+}

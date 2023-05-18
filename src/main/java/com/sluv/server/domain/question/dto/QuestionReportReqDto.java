70acd7a53625d00ddd6865fd295a09cdc3ed2dd6
junker <kjgi73k@gmail.com>
2023-05-18 15:51:21 +0900

    [#125] feat : Question 신고를 위한 Dto 생성
    - QuestionReportReqDto 생성

---
+++ src/main/java/com/sluv/server/domain/question/dto/QuestionReportReqDto.java
@@ -0,0 +1,16 @@
+package com.sluv.server.domain.question.dto;
+
+import com.sluv.server.domain.question.enums.QuestionReportReason;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class QuestionReportReqDto {
+    private QuestionReportReason reportReason;
+    private String content;
+}

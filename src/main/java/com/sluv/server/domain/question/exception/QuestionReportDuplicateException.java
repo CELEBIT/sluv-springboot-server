d00c74e127824d82655e8776ece998aa6ad1c4b5
junker <kjgi73k@gmail.com>
2023-05-18 15:50:46 +0900

    [#125] feat : 중복 신고 방지 예외 생성
    - QuestionReportDuplicateException 생성

---
+++ src/main/java/com/sluv/server/domain/question/exception/QuestionReportDuplicateException.java
@@ -0,0 +1,15 @@
+package com.sluv.server.domain.question.exception;
+
+import com.sluv.server.domain.item.exception.ItemException;
+import org.springframework.http.HttpStatus;
+
+public class QuestionReportDuplicateException extends ItemException {
+    private static final int ERROR_CODE = 2013;
+    private static final String MESSAGE = "중복된 Question 게시글 신고입니다.";
+    private static final HttpStatus STATUS = HttpStatus.BAD_REQUEST;
+
+    public QuestionReportDuplicateException() {
+        super(ERROR_CODE, STATUS, MESSAGE);
+    }
+
+}

9c83df2886c0edfef2197ff38a6227ee3b21faef
junker <kjgi73k@gmail.com>
2023-05-19 15:04:29 +0900

    [#131] feat : 댓글 수정 기능 추가

--- src/main/java/com/sluv/server/domain/comment/repository/CommentImgRepository.java
+++ src/main/java/com/sluv/server/domain/comment/repository/CommentImgRepository.java
@@ -4,4 +4,5 @@ import com.sluv.server.domain.comment.entity.CommentImg;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentImgRepository extends JpaRepository<CommentImg, Long> {
+    void deleteAllByCommentId(Long commentId);
 }

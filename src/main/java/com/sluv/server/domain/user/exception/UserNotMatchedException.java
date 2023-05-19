c5d40ef1a1665018b8788d39a80a458629f8bfd0
junker <kjgi73k@gmail.com>
2023-05-19 15:03:05 +0900

    feat : UserNotMatchedException 추가

---
+++ src/main/java/com/sluv/server/domain/user/exception/UserNotMatchedException.java
@@ -0,0 +1,14 @@
+package com.sluv.server.domain.user.exception;
+
+import com.sluv.server.domain.auth.exception.AuthException;
+import org.springframework.http.HttpStatus;
+
+public class UserNotMatchedException extends AuthException {
+    private static final int ERROR_CODE = 2015;
+    private static final String MESSAGE = "유저 정보가 일치하지 않습니다.";
+    private static final HttpStatus STATUS = HttpStatus.BAD_REQUEST;
+
+    public UserNotMatchedException() {
+        super(ERROR_CODE, STATUS, MESSAGE);
+    }
+}

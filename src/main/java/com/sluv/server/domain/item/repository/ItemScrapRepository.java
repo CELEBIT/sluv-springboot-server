a727b33a0f6e830ccca2fe76fa7ae8c5f0862a82
junker <kjgi73k@gmail.com>
2023-05-30 23:31:53 +0900

    [#156] feat : 편집하기를 통한 Closet의 Item 삭제 로직 추가

--- src/main/java/com/sluv/server/domain/item/repository/ItemScrapRepository.java
+++ src/main/java/com/sluv/server/domain/item/repository/ItemScrapRepository.java
@@ -3,6 +3,10 @@ package com.sluv.server.domain.item.repository;
 import com.sluv.server.domain.item.entity.ItemScrap;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemScrapRepository extends JpaRepository<ItemScrap, Long> {
     void deleteAllByClosetId(Long id);
+
+    void deleteByClosetIdAndItemId(Long closet_id, Long item_id);
 }

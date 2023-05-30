11653e1d994530afe98489ab2e7cca9ab8de6f4d
junker <kjgi73k@gmail.com>
2023-05-30 16:56:19 +0900

    [#150] feat : Dto Fix
    - ClosetReqDto를 ClosetReqDto로 이름 변경

--- src/main/java/com/sluv/server/domain/closet/dto/ClosetReqDto.java
+++ src/main/java/com/sluv/server/domain/closet/dto/ClosetReqDto.java
@@ -1,6 +1,7 @@
 package com.sluv.server.domain.closet.dto;
 
 import com.sluv.server.domain.closet.enums.ClosetStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,10 +11,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class ClosetPostReqDto {
+public class ClosetReqDto {
+    @Schema(description = "Closet 이름")
     private String name;
+    @Schema(description = "Closet 커버 사진 URL")
     private String coverImgUrl;
+    @Schema(description = "Closet 공개 여부")
     private ClosetStatus closetStatus;
+    @Schema(description = "Closet 커버 색상")
     private String color;
 
 }

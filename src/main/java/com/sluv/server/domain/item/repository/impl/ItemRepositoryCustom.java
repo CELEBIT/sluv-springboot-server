027800a16588ba059d6cca921fe40346562d6b72
junker <kjgi73k@gmail.com>
2023-06-05 18:41:52 +0900

    [#162] feat : Repository method add

--- src/main/java/com/sluv/server/domain/item/repository/impl/ItemRepositoryCustom.java
+++ src/main/java/com/sluv/server/domain/item/repository/impl/ItemRepositoryCustom.java
@@ -1,6 +1,10 @@
 package com.sluv.server.domain.item.repository.impl;
 
 import com.sluv.server.domain.item.entity.Item;
+import com.sluv.server.domain.item.entity.RecentItem;
+import com.sluv.server.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -9,4 +13,6 @@ public interface ItemRepositoryCustom {
     List<Item> findSameCelebItem(Item item, boolean celebJudge);
 
     List<Item> findSameBrandItem(Item item, boolean brandJudge);
+
+    Page<Item> getRecentItem(User user, Pageable pageable);
 }

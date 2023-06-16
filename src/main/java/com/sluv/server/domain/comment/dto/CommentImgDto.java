ee319c5a5b487e8cfd5b45eece91318e81e71aa3
junker <kjgi73k@gmail.com>
2023-06-16 20:50:17 +0900

    [#175] feat : Comment Img, Item 순서 추가를 위한 Dto 수정 및 생성
    - CommentPostReqDto 수정
    - CommentResDto 수정
    
    - CommentImgDto 생성
    - CommentItemReqDto 생성
    - CommentItemResDto 생성

---
+++ src/main/java/com/sluv/server/domain/comment/dto/CommentImgDto.java
@@ -0,0 +1,18 @@
+package com.sluv.server.domain.comment.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CommentImgDto {
+    @Schema(description = "Comment 이미지 Url")
+    private String imgUrl;
+    @Schema(description = "Comment 이미지 순서")
+    private Integer order;
+}

6d1acf80d9f7070ca503b56dd0b7af9dc339c596
junker <kjgi73k@gmail.com>
2023-06-16 21:01:57 +0900

    [#175] feat : ItemImg 순서 추가를 위한 Dto 수정
    - ItemImgResDto 수정

--- src/main/java/com/sluv/server/domain/item/dto/ItemImgResDto.java
+++ src/main/java/com/sluv/server/domain/item/dto/ItemImgResDto.java
@@ -13,5 +13,7 @@ public class ItemImgResDto {
     private String imgUrl;
     @Schema(description = "대표 이미지 여부(true or false)")
     private Boolean representFlag;
+    @Schema(description = "이미지 순서")
+    private Integer order;
 
 }

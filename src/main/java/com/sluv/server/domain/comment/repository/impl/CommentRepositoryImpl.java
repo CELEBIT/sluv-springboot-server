6145eb3c797f969ab36e87e58356154d33ae7dc4
junker <kjgi73k@gmail.com>
2023-06-22 16:30:23 +0900

    fix : 댓글 검색 로직 수정
    - Comment의 Parent_id가 null인 것만 검색

--- src/main/java/com/sluv/server/domain/comment/repository/impl/CommentRepositoryImpl.java
+++ src/main/java/com/sluv/server/domain/comment/repository/impl/CommentRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.sluv.server.domain.comment.repository.impl;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sluv.server.domain.comment.entity.Comment;
+import com.sluv.server.domain.comment.enums.CommentStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,7 +20,10 @@ public class CommentRepositoryImpl implements CommentRepositoryCustom{
     @Override
     public Page<Comment> getAllQuestionComment(Long questionId, Pageable pageable) {
         List<Comment> content = jpaQueryFactory.selectFrom(comment)
-                .where(comment.question.id.eq(questionId))
+                .where(comment.question.id.eq(questionId)
+                        .and(comment.parent.isNull())
+                        .and(comment.commentStatus.eq(CommentStatus.ACTIVE))
+                )
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
@@ -35,7 +39,9 @@ public class CommentRepositoryImpl implements CommentRepositoryCustom{
     @Override
     public Page<Comment> getAllSubComment(Long commentId, Pageable pageable) {
         List<Comment> content = jpaQueryFactory.selectFrom(comment)
-                .where(comment.parent.id.eq(commentId))
+                .where(comment.parent.id.eq(commentId)
+                        .and(comment.commentStatus.eq(CommentStatus.ACTIVE))
+                )
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();

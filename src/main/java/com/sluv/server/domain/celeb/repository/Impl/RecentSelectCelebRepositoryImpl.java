c9fb967bfb38a40afcaf3033e03324adcd293b70
KJBig <kjgi73k@gmail.com>
2023-11-24 22:05:31 +0900

    S2D-55 refactor : 최근 검색한 셀럽 조회 성능 개선.

--- src/main/java/com/sluv/server/domain/celeb/repository/Impl/RecentSelectCelebRepositoryImpl.java
+++ src/main/java/com/sluv/server/domain/celeb/repository/Impl/RecentSelectCelebRepositoryImpl.java
@@ -1,13 +1,14 @@
 package com.sluv.server.domain.celeb.repository.Impl;
 
+import static com.sluv.server.domain.celeb.entity.QCeleb.celeb;
+import static com.sluv.server.domain.celeb.entity.QNewCeleb.newCeleb;
+import static com.sluv.server.domain.celeb.entity.QRecentSelectCeleb.recentSelectCeleb;
+
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sluv.server.domain.celeb.entity.RecentSelectCeleb;
 import com.sluv.server.domain.user.entity.User;
-import lombok.RequiredArgsConstructor;
-
 import java.util.List;
-
-import static com.sluv.server.domain.celeb.entity.QRecentSelectCeleb.recentSelectCeleb;
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class RecentSelectCelebRepositoryImpl implements RecentSelectCelebRepositoryCustom {
@@ -17,6 +18,8 @@ public class RecentSelectCelebRepositoryImpl implements RecentSelectCelebReposit
     @Override
     public List<RecentSelectCeleb> getRecentSelectCelebTop20(User user) {
         return jpaQueryFactory.selectFrom(recentSelectCeleb)
+                .leftJoin(recentSelectCeleb.celeb, celeb).fetchJoin()
+                .leftJoin(recentSelectCeleb.newCeleb, newCeleb).fetchJoin()
                 .where(recentSelectCeleb.user.eq(user))
                 .limit(20)
                 .groupBy(recentSelectCeleb.celeb, recentSelectCeleb.newCeleb)

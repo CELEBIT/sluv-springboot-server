140ed614bfdd363562eedefc82d2111074feac52
KJBig <kjgi73k@gmail.com>
2023-11-28 14:08:18 +0900

    S2D-55  refactor : UserController를 분리
    - 다음과 같은 Controller로 분리
    1. FollowController
    2. UserCelebController
    3. UserLikeController
    4. UserRecentController
    5. UserReportController

--- src/main/java/com/sluv/server/domain/user/controller/UserController.java
+++ src/main/java/com/sluv/server/domain/user/controller/UserController.java
@@ -1,9 +1,6 @@
 package com.sluv.server.domain.user.controller;
 
 
-import com.sluv.server.domain.celeb.dto.InterestedCelebCategoryResDto;
-import com.sluv.server.domain.celeb.dto.InterestedCelebParentResDto;
-import com.sluv.server.domain.celeb.dto.InterestedCelebPostReqDto;
 import com.sluv.server.domain.closet.dto.ClosetResDto;
 import com.sluv.server.domain.comment.dto.CommentSimpleResDto;
 import com.sluv.server.domain.item.dto.ItemSimpleResDto;
@@ -11,21 +8,14 @@ import com.sluv.server.domain.question.dto.QuestionSimpleResDto;
 import com.sluv.server.domain.user.dto.UserMypageResDto;
 import com.sluv.server.domain.user.dto.UserProfileImgReqDto;
 import com.sluv.server.domain.user.dto.UserProfileReqDto;
-import com.sluv.server.domain.user.dto.UserReportReqDto;
 import com.sluv.server.domain.user.dto.UserSearchInfoDto;
 import com.sluv.server.domain.user.entity.User;
-import com.sluv.server.domain.user.service.UserReportService;
 import com.sluv.server.domain.user.service.UserService;
-import com.sluv.server.global.common.response.ErrorResponse;
 import com.sluv.server.global.common.response.PaginationCountResDto;
 import com.sluv.server.global.common.response.PaginationResDto;
 import com.sluv.server.global.common.response.SuccessDataResponse;
 import com.sluv.server.global.common.response.SuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.annotation.Nullable;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -49,85 +39,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/app/user")
 public class UserController {
     private final UserService userService;
-    private final UserReportService userReportService;
-
-    @Operation(
-            summary = "*현재 유저의 관심 샐럽을 카테고리를 기준으로 조회",
-            description = "현재 유저를 기준으로 InterstedCeleb 테이블에서 일치하는 Celeb을 카테고리를 기준으로 검색"
-    )
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "1000", description = "요청성공"),
-            @ApiResponse(responseCode = "5000", description = "서버내부 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "5001", description = "DB 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
-    })
-    @GetMapping("/celeb/category")
-    public ResponseEntity<SuccessDataResponse<List<InterestedCelebCategoryResDto>>> getInterestedCelebByCategory(
-            @AuthenticationPrincipal User user) {
-
-        return ResponseEntity.ok().body(SuccessDataResponse.<List<InterestedCelebCategoryResDto>>builder()
-                .result(userService.getInterestedCelebByCategory(user))
-                .build());
-    }
-
-    @Operation(
-            summary = "*유저 팔로우/팔로잉",
-            description = "유저의 팔로우/팔로잉 등록" +
-                    "\n (User Id Token 필요)"
-    )
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "1000", description = "요청성공"),
-            @ApiResponse(responseCode = "5000", description = "서버내부 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "5001", description = "DB 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
-    })
-    @PostMapping("/{userId}/follow")
-    public ResponseEntity<SuccessResponse> postUserFollow(@AuthenticationPrincipal User user,
-                                                          @PathVariable(name = "userId") Long userId) {
-        userService.postUserFollow(user, userId);
-        return ResponseEntity.ok().body(
-                new SuccessResponse()
-        );
-    }
-
-    @Operation(
-            summary = "*유저 신고하기",
-            description = "유저를 신고하는 기능" +
-                    "\n (User Id 필요)"
-    )
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "1000", description = "요청성공"),
-            @ApiResponse(responseCode = "5000", description = "서버내부 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "5001", description = "DB 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
-    })
-    @PostMapping("/{userId}/report")
-    public ResponseEntity<SuccessResponse> postUserReport(@AuthenticationPrincipal User user,
-                                                          @PathVariable(name = "userId") Long userId,
-                                                          @RequestBody UserReportReqDto dto) {
-        userReportService.postUserReport(user, userId, dto);
-        return ResponseEntity.ok().body(
-                new SuccessResponse()
-        );
-    }
-
-    @Operation(
-            summary = "*유저의 관심 셀럽 업데이트",
-            description = "유저의 관심 셀럽 목록을 업데이트" +
-                    "\n 1. User Id를 기준으로 InterestedCeleb 테이블의 모든 데이터 삭제" +
-                    "\n 2. User Id와 Dto의 정보를 바탕으로 정보 push" +
-                    "\n (User Id Token 필요)"
-    )
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "1000", description = "요청성공"),
-            @ApiResponse(responseCode = "5000", description = "서버내부 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "5001", description = "DB 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
-    })
-    @PostMapping("/celeb")
-    public ResponseEntity<SuccessResponse> postInterestedCeleb(@AuthenticationPrincipal User user,
-                                                               @RequestBody InterestedCelebPostReqDto dto) {
-        userService.postInterestedCeleb(user, dto);
-        return ResponseEntity.ok().body(
-                new SuccessResponse()
-        );
-    }
 
     @Operation(
             summary = "*유저의 프로필 설정 및 수정",
@@ -204,7 +115,7 @@ public class UserController {
     }
 
     @Operation(
-            summary = "특정 유저의 옷장 목록 조회",
+            summary = "*특정 유저의 옷장 목록 조회",
             description = """
                     특정 유저의 옷장 목록 조회\n
                     User Id Token 필요
@@ -224,107 +135,7 @@ public class UserController {
     }
 
     @Operation(
-            summary = "유저의 최근 본 아이템 조회",
-            description = """
-                    유저의 최근 본 아이템 조회\n
-                    User Id Token 필요
-                        -> Id를 기준으로 조회\n
-                    Pagination 적용\n
-                    """
-    )
-    @GetMapping("/recent/item")
-    public ResponseEntity<SuccessDataResponse<PaginationCountResDto<ItemSimpleResDto>>> getUserRecentItem(
-            @AuthenticationPrincipal User user, Pageable pageable) {
-
-        return ResponseEntity.ok().body(
-                SuccessDataResponse.<PaginationCountResDto<ItemSimpleResDto>>builder()
-                        .result(userService.getUserRecentItem(user, pageable))
-                        .build()
-        );
-    }
-
-    @Operation(
-            summary = "유저의 최근 본 Question 조회",
-            description = """
-                    유저의 최근 본 Question 조회\n
-                    User Id Token 필요
-                        -> Id를 기준으로 조회\n
-                    Pagination 적용\n
-                    """
-    )
-    @GetMapping("/recent/question")
-    public ResponseEntity<SuccessDataResponse<PaginationCountResDto<QuestionSimpleResDto>>> getUserRecentQuestion(
-            @AuthenticationPrincipal User user, Pageable pageable) {
-
-        return ResponseEntity.ok().body(
-                SuccessDataResponse.<PaginationCountResDto<QuestionSimpleResDto>>builder()
-                        .result(userService.getUserRecentQuestion(user, pageable))
-                        .build()
-        );
-    }
-
-    @Operation(
-            summary = "유저가 좋아요한 아이템 조회",
-            description = """
-                    유저가 좋아요한 아이템 조회\n
-                    User Id Token 필요
-                        -> Id를 기준으로 조회\n
-                    Pagination 적용\n
-                    """
-    )
-    @GetMapping("/like/item")
-    public ResponseEntity<SuccessDataResponse<PaginationCountResDto<ItemSimpleResDto>>> getUserLikeItem(
-            @AuthenticationPrincipal User user, Pageable pageable) {
-
-        return ResponseEntity.ok().body(
-                SuccessDataResponse.<PaginationCountResDto<ItemSimpleResDto>>builder()
-                        .result(userService.getUserLikeItem(user, pageable))
-                        .build()
-        );
-    }
-
-    @Operation(
-            summary = "특정 유저를 등록한 팔로워들 조회",
-            description = """
-                    특정 유저를 좋아요한 아이템 조회\n
-                    User Id Token 필요
-                        -> 현재 유저가 팔로워를 팔로잉 했는지 확인\n
-                    Pagination 적용\n
-                    """
-    )
-    @GetMapping("/{userId}/follower")
-    public ResponseEntity<SuccessDataResponse<PaginationResDto<UserSearchInfoDto>>> getUserFollower(
-            @AuthenticationPrincipal User user, @PathVariable("userId") Long userId, Pageable pageable) {
-
-        return ResponseEntity.ok().body(
-                SuccessDataResponse.<PaginationResDto<UserSearchInfoDto>>builder()
-                        .result(userService.getUserFollower(user, userId, pageable))
-                        .build()
-        );
-    }
-
-    @Operation(
-            summary = "특정 유저가 등록한 팔로잉 조회",
-            description = """
-                    특정 유저가 등록한 팔로잉 조회\n
-                    User Id Token 필요
-                        -> 현재 유저가 팔로워를 팔로잉 했는지 확인\n
-                    Pagination 적용\n
-                    """
-    )
-    @GetMapping("/{userId}/following")
-    public ResponseEntity<SuccessDataResponse<PaginationResDto<UserSearchInfoDto>>> getUserFollowing(
-            @AuthenticationPrincipal User user, @PathVariable("userId") Long userId, Pageable pageable) {
-
-        return ResponseEntity.ok().body(
-                SuccessDataResponse.<PaginationResDto<UserSearchInfoDto>>builder()
-                        .result(userService.getUserFollowing(user, userId, pageable))
-                        .build()
-        );
-    }
-
-    @Operation(
-            summary = "유저의 프로필 이미지 수정",
+            summary = "*유저의 프로필 이미지 수정",
             description = """
                     유저의 프로필 이미지 수정\n
                     User Id Token 필요
@@ -343,7 +154,7 @@ public class UserController {
     }
 
     @Operation(
-            summary = "유저의 프로필 이미지 삭제",
+            summary = "*유저의 프로필 이미지 삭제",
             description = """
                     유저의 프로필 이미지 삭제\n
                     null로 변경 \n
@@ -362,47 +173,7 @@ public class UserController {
     }
 
     @Operation(
-            summary = "유저가 좋아요한 Question 게시글 조회",
-            description = """
-                    유저가 좋아요한 Question 게시글 조회\n
-                    User Id Token 필요
-                        -> Id를 기준으로 조회\n
-                    Pagination 적용\n
-                    """
-    )
-    @GetMapping("/like/question")
-    public ResponseEntity<SuccessDataResponse<PaginationCountResDto<QuestionSimpleResDto>>> getUserLikeQuestion(
-            @AuthenticationPrincipal User user, Pageable pageable) {
-
-        return ResponseEntity.ok().body(
-                SuccessDataResponse.<PaginationCountResDto<QuestionSimpleResDto>>builder()
-                        .result(userService.getUserLikeQuestion(user, pageable))
-                        .build()
-        );
-    }
-
-    @Operation(
-            summary = "유저가 좋아요한 Comment 조회",
-            description = """
-                    유저가 좋아요한 Comment 조회\n
-                    User Id Token 필요\n
-                        -> Id를 기준으로 조회\n
-                    Pagination 적용\n
-                    """
-    )
-    @GetMapping("/like/comment")
-    public ResponseEntity<SuccessDataResponse<PaginationCountResDto<CommentSimpleResDto>>> getUserLikeComment(
-            @AuthenticationPrincipal User user, Pageable pageable) {
-
-        return ResponseEntity.ok().body(
-                SuccessDataResponse.<PaginationCountResDto<CommentSimpleResDto>>builder()
-                        .result(userService.getUserLikeComment(user, pageable))
-                        .build()
-        );
-    }
-
-    @Operation(
-            summary = "유저가 작성한 Item 게시글 조회",
+            summary = "*유저가 작성한 Item 게시글 조회",
             description = """
                     유저가 작성한 Item 게시글 조회\n
                     User Id Token 필요\n
@@ -422,7 +193,7 @@ public class UserController {
     }
 
     @Operation(
-            summary = "유저가 작성한 Question 게시글 조회",
+            summary = "*유저가 작성한 Question 게시글 조회",
             description = """
                     유저가 작성한 Question 게시글 조회\n
                     User Id Token 필요\n
@@ -442,7 +213,7 @@ public class UserController {
     }
 
     @Operation(
-            summary = "유저가 작성한 Comment 게시글 조회",
+            summary = "*유저가 작성한 Comment 게시글 조회",
             description = """
                     유저가 작성한 Comment 게시글 조회\n
                     User Id Token 필요\n
@@ -486,82 +257,5 @@ public class UserController {
                         .result(userService.getHotSluver(user, celebId))
                         .build()
         );
-    }
-
-    @Operation(
-            summary = "*특정 유저의 관심 샐럽을 카테고리를 기준으로 조회",
-            description = "특정 유저를 기준으로 InterstedCeleb 테이블에서 일치하는 Celeb을 카테고리를 기준으로 검색"
-    )
-    @GetMapping("/{userId}/celeb/category")
-    public ResponseEntity<SuccessDataResponse<List<InterestedCelebCategoryResDto>>> getTargetUserInterestedCelebByCategory(
-            @PathVariable("userId") Long userId) {
-
-        return ResponseEntity.ok().body(SuccessDataResponse.<List<InterestedCelebCategoryResDto>>builder()
-                .result(userService.getTargetUserInterestedCelebByCategory(userId))
-                .build());
-    }
-
-    @Operation(
-            summary = "*현재 유저를 등록한 팔로워들 조회",
-            description = """
-                    현재 유저를 좋아요한 아이템 조회\n
-                    User Id Token 필요
-                    Pagination 적용\n
-                    """
-    )
-    @GetMapping("/follower")
-    public ResponseEntity<SuccessDataResponse<PaginationResDto<UserSearchInfoDto>>> getNowUserFollower(
-            @AuthenticationPrincipal User user, Pageable pageable) {
-
-        return ResponseEntity.ok().body(
-                SuccessDataResponse.<PaginationResDto<UserSearchInfoDto>>builder()
-                        .result(userService.getUserFollower(user, user.getId(), pageable))
-                        .build()
-        );
-    }
-
-    @Operation(
-            summary = "*현재 유저가 등록한 팔로잉 조회",
-            description = """
-                    *현재 유저가 등록한 팔로잉 조회\n
-                    User Id Token 필요
-                    Pagination 적용\n
-                    """
-    )
-    @GetMapping("/following")
-    public ResponseEntity<SuccessDataResponse<PaginationResDto<UserSearchInfoDto>>> getNowUserFollowing(
-            @AuthenticationPrincipal User user, Pageable pageable) {
-
-        return ResponseEntity.ok().body(
-                SuccessDataResponse.<PaginationResDto<UserSearchInfoDto>>builder()
-                        .result(userService.getUserFollowing(user, user.getId(), pageable))
-                        .build()
-        );
-    }
-
-    @Operation(
-            summary = "특정 유저의 관심 샐럽을 등록순을 기준으로 조회",
-            description = "특정 유저를 기준으로 InterstedCeleb 테이블에서 일치하는 Celeb을 등록순을 기준으로 검색"
-    )
-    @GetMapping("/{userId}/celeb")
-    public ResponseEntity<SuccessDataResponse<List<InterestedCelebParentResDto>>> getTargetUserInterestedCelebByPostTime(
-            @PathVariable("userId") Long userId) {
-
-        return ResponseEntity.ok().body(SuccessDataResponse.<List<InterestedCelebParentResDto>>builder()
-                .result(userService.getTargetUserInterestedCelebByPostTime(userId))
-                .build());
-    }
-
-    @Operation(
-            summary = "*현재 유저의 관심 샐럽을 등록순을 기준으로 조회",
-            description = "현재 유저를 기준으로 InterstedCeleb 테이블에서 일치하는 Celeb을 등록순을 기준으로 검색"
-    )
-    @GetMapping("/celeb")
-    public ResponseEntity<SuccessDataResponse<List<InterestedCelebParentResDto>>> getInterestedCelebByPostTime(
-            @AuthenticationPrincipal User user) {
-
-        return ResponseEntity.ok().body(SuccessDataResponse.<List<InterestedCelebParentResDto>>builder()
-                .result(userService.getInterestedCelebByPostTime(user))
-                .build());
     }
 }

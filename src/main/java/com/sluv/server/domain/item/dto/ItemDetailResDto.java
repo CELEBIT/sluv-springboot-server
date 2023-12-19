e2886dd75088682a59500d1836bb97afcb867148
KJBig <kjgi73k@gmail.com>
2023-12-19 18:16:05 +0900

    feat : ItemDetailResDto에 컬러값 추가.

--- src/main/java/com/sluv/server/domain/item/dto/ItemDetailResDto.java
+++ src/main/java/com/sluv/server/domain/item/dto/ItemDetailResDto.java
@@ -121,6 +121,7 @@ public class ItemDetailResDto {
                 .otherSluverItemList(otherSluverItemList)
                 .followStatus(followStatus)
                 .hasMine(hasMine)
+                .color(item.getColor())
                 .build();
     }
 

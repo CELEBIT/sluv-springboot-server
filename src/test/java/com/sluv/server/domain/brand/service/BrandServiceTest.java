9165a38140473f436533edf372583afa3bdb25ab
KJBig <kjgi73k@gmail.com>
2024-05-21 15:18:17 +0900

    fix : TEST 수정

--- src/test/java/com/sluv/server/domain/brand/service/BrandServiceTest.java
+++ src/test/java/com/sluv/server/domain/brand/service/BrandServiceTest.java
@@ -4,6 +4,7 @@ import static com.sluv.server.fixture.UserFixture.카카오_유저_생성;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.sluv.server.domain.brand.dto.BrandSearchResDto;
+import com.sluv.server.domain.brand.dto.RecentSelectBrandResDto;
 import com.sluv.server.domain.brand.entity.Brand;
 import com.sluv.server.domain.brand.entity.RecentSelectBrand;
 import com.sluv.server.domain.brand.repository.BrandRepository;
@@ -104,30 +105,30 @@ public class BrandServiceTest {
         assertThat(topBrand).hasSize(10);
     }
 
-//    @DisplayName("최근 선택한 브랜드를 조회할 수 있다.")
-//    @Test
-//    void findRecentSelectBrandTest() {
-//        //given
-//        User user = userRepository.save(카카오_유저_생성());
-//        Brand brand1 = Brand.builder().brandKr("브랜드1").brandEn("brand1").build();
-//        Brand brand2 = Brand.builder().brandKr("브랜드2").brandEn("brand2").build();
-//        Brand brand3 = Brand.builder().brandKr("브랜드3").brandEn("brand3").build();
-//        List<Brand> brands = brandRepository.saveAll(List.of(brand1, brand2, brand3));
-//
-//        RecentSelectBrand recentSelectBrand1 = RecentSelectBrand.toEntity(brands.get(0), null, user);
-//        RecentSelectBrand recentSelectBrand2 = RecentSelectBrand.toEntity(brands.get(1), null, user);
-//        RecentSelectBrand recentSelectBrand3 = RecentSelectBrand.toEntity(brands.get(2), null, user);
-//        RecentSelectBrand recentSelectBrand4 = RecentSelectBrand.toEntity(brands.get(0), null, user);
-//        List<RecentSelectBrand> recentSelectBrands = List.of(recentSelectBrand1, recentSelectBrand2,
-//                recentSelectBrand3, recentSelectBrand4);
-//
-//        recentSelectBrandRepository.saveAll(recentSelectBrands);
-//
-//        //when
-//        List<RecentSelectBrandResDto> recentSelectBrand = brandService.findRecentSelectBrand(user);
-//
-//        //then
-//        assertThat(recentSelectBrand.get(0)).extracting("brandName").isEqualTo("브랜드1");
-//        assertThat(recentSelectBrand.get(1)).extracting("brandName").isEqualTo("브랜드3");
-//    }
+    @DisplayName("최근 선택한 브랜드를 조회할 수 있다.")
+    @Test
+    void findRecentSelectBrandTest() {
+        //given
+        User user = userRepository.save(카카오_유저_생성());
+        Brand brand1 = Brand.builder().brandKr("브랜드1").brandEn("brand1").build();
+        Brand brand2 = Brand.builder().brandKr("브랜드2").brandEn("brand2").build();
+        Brand brand3 = Brand.builder().brandKr("브랜드3").brandEn("brand3").build();
+        List<Brand> brands = brandRepository.saveAll(List.of(brand1, brand2, brand3));
+
+        RecentSelectBrand recentSelectBrand1 = RecentSelectBrand.toEntity(brands.get(0), null, user);
+        RecentSelectBrand recentSelectBrand2 = RecentSelectBrand.toEntity(brands.get(1), null, user);
+        RecentSelectBrand recentSelectBrand3 = RecentSelectBrand.toEntity(brands.get(2), null, user);
+        RecentSelectBrand recentSelectBrand4 = RecentSelectBrand.toEntity(brands.get(0), null, user);
+        List<RecentSelectBrand> recentSelectBrands = List.of(recentSelectBrand1, recentSelectBrand2,
+                recentSelectBrand3, recentSelectBrand4);
+
+        recentSelectBrandRepository.saveAll(recentSelectBrands);
+
+        //when
+        List<RecentSelectBrandResDto> recentSelectBrand = brandService.findRecentSelectBrand(user);
+
+        //then
+        assertThat(recentSelectBrand.get(0)).extracting("brandName").isEqualTo("브랜드1");
+        assertThat(recentSelectBrand.get(1)).extracting("brandName").isEqualTo("브랜드3");
+    }
 }

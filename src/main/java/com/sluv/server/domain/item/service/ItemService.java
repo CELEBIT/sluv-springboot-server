d9be3e2e3d3ec5cf1fb45c8a16e47fc60c36e07f
KJBig <kjgi73k@gmail.com>
2024-06-30 18:54:31 +0900

    feat : 내 글에 하트가 달렸을 때 알람 발생
    - (유저)님이 회원님이 올린 아이템 정보를 좋아해요
    - 클릭 시, 해당 게시글로 이동

--- src/main/java/com/sluv/server/domain/item/service/ItemService.java
+++ src/main/java/com/sluv/server/domain/item/service/ItemService.java
@@ -1,5 +1,6 @@
 package com.sluv.server.domain.item.service;
 
+import com.sluv.server.domain.alarm.service.AlarmService;
 import com.sluv.server.domain.brand.dto.BrandSearchResDto;
 import com.sluv.server.domain.brand.entity.Brand;
 import com.sluv.server.domain.brand.entity.NewBrand;
@@ -90,6 +91,7 @@ public class ItemService {
 
     private final AiModelService aiModelService;
     private final CacheService cacheService;
+    private final AlarmService alarmService;
 
     @Transactional
     public ItemPostResDto postItem(User user, ItemPostReqDto reqDto) {
@@ -325,9 +327,8 @@ public class ItemService {
 
         boolean likeExist = itemLikeRepository.existsByUserIdAndItemId(user.getId(), itemId);
         if (!likeExist) {
-            itemLikeRepository.save(
-                    ItemLike.toEntity(item, user)
-            );
+            itemLikeRepository.save(ItemLike.toEntity(item, user));
+            alarmService.sendAlarmAboutItemLike(user.getId(), itemId);
         } else {
             itemLikeRepository.deleteByUserIdAndItemId(user.getId(), itemId);
         }

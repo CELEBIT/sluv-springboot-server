9ad3b9a8f3f9f0204ccd0dee13456efb8145114c
KJBig <kjgi73k@gmail.com>
2024-06-30 18:42:07 +0900

    fix : Hashtag 예외 코드 변경

--- src/main/java/com/sluv/server/domain/item/exception/hashtag/HashtagNotFoundException.java
+++ src/main/java/com/sluv/server/domain/item/exception/hashtag/HashtagNotFoundException.java
@@ -3,8 +3,8 @@ package com.sluv.server.domain.item.exception.hashtag;
 import org.springframework.http.HttpStatus;
 
 public class HashtagNotFoundException extends HashtagException {
-    private static final int ERROR_CODE = 2003;
-    private static final String MESSAGE = "존재하지 않는 아이템 카테고리입니다.";
+    private static final int ERROR_CODE = 2029;
+    private static final String MESSAGE = "존재하지 않는 Hashtag입니다.";
     private static final HttpStatus STATUS = HttpStatus.BAD_REQUEST;
 
     public HashtagNotFoundException() {

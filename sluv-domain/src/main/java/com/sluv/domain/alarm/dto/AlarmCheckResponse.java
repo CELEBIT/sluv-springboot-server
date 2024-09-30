fedb6605971e0cd25af3c2050c0e3e9b781e687b
KJBig <kjgi73k@gmail.com>
2024-09-30 09:31:06 +0900

    fix : 알람 에러 해결

---
+++ sluv-domain/src/main/java/com/sluv/domain/alarm/dto/AlarmCheckResponse.java
@@ -0,0 +1,19 @@
+package com.sluv.domain.alarm.dto;
+
+import lombok.*;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class AlarmCheckResponse {
+
+    private Boolean isAllRead;
+
+    public static AlarmCheckResponse of(Boolean isAllRead) {
+        return AlarmCheckResponse.builder()
+                .isAllRead(isAllRead)
+                .build();
+    }
+
+}

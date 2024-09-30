695b2db7fb1cbf8704fa079ec0ee985d6681fe48
KJBig <kjgi73k@gmail.com>
2024-09-30 09:12:35 +0900

    feat : Cors 설정 주소 추가. - Dev용 React 주소 추가.

--- sluv-api/src/main/java/com/sluv/api/config/security/CorsConfig.java
+++ sluv-api/src/main/java/com/sluv/api/config/security/CorsConfig.java
@@ -1,11 +1,12 @@
 package com.sluv.api.config.security;
 
-import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
@@ -14,7 +15,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(
-                Arrays.asList("https://www.sluv.co.kr", "http://localhost:3000", "https://sluv-server.co.kr"));
+                Arrays.asList("https://www.sluv.co.kr", "http://localhost:3000", "https://sluv-server.co.kr", "https://dev.sluv.co.kr"));
         configuration.addAllowedHeader("Authorization");
         configuration.addAllowedHeader("Content-Type");
         configuration.addAllowedMethod("*");

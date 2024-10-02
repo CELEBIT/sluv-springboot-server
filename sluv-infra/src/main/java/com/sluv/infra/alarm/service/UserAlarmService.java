0746f13d98f7d14aa1e2aebca044f8adfccc0b85
KJBig <kjgi73k@gmail.com>
2024-10-02 15:25:05 +0900

    fix : 알람 타입 수정
    - Question -> User 오타 수정

--- sluv-infra/src/main/java/com/sluv/infra/alarm/service/UserAlarmService.java
+++ sluv-infra/src/main/java/com/sluv/infra/alarm/service/UserAlarmService.java
@@ -37,7 +37,7 @@ public class UserAlarmService {
 
     private void sendMessageTypeUser(User receiver, User sender, String message) {
         AlarmElement alarmElement = AlarmElement.of(null, null, null, sender);
-        alarmDomainService.saveAlarm(receiver, ALARM_TITLE, message, AlarmType.QUESTION, alarmElement);
+        alarmDomainService.saveAlarm(receiver, ALARM_TITLE, message, AlarmType.USER, alarmElement);
         fcmNotificationService.sendFCMNotification(
                 receiver.getId(), ALARM_TITLE, message, AlarmType.USER, getIdAboutUser(sender.getId())
         );

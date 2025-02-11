3828ccffd363779aa72455bf9729cb7ab7d7c911
KIM JUNGI <101792740+KJBig@users.noreply.github.com>
2025-02-11 13:23:08 +0900

    feat : UserReportService 테스트 코드 작성
    - 기존 통합 테스트 -> 단위 테스트로 변경.

--- sluv-api/src/test/java/com/sluv/api/domain/user/service/UserReportServiceTest.java
+++ sluv-api/src/test/java/com/sluv/api/domain/user/service/UserReportServiceTest.java
@@ -1,54 +1,53 @@
-//package com.sluv.server.domain.user.service;
-//
-//import static com.sluv.server.domain.user.enums.UserReportReason.BAD_MANNER;
-//import static com.sluv.server.fixture.UserFixture.구글_유저_생성;
-//import static com.sluv.server.fixture.UserFixture.카카오_유저_생성;
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//import com.sluv.server.domain.user.dto.UserReportReqDto;
-//import com.sluv.server.domain.user.entity.User;
-//import com.sluv.server.domain.user.repository.UserReportRepository;
-//import com.sluv.server.domain.user.repository.UserRepository;
-//import java.util.List;
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//public class UserReportServiceTest {
-//
-//    @Autowired
-//    private UserReportService userReportService;
-//
-//    @Autowired
-//    private UserReportRepository userReportRepository;
-//
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//    @AfterEach
-//    void clear() {
-//        userReportRepository.deleteAll();
-//        userRepository.deleteAll();
-//    }
-//
-//    @DisplayName("사용자를 신고한다.")
-//    @Test
-//    void postUserReportTest() {
-//        //given
-//        User user1 = 카카오_유저_생성();
-//        User user2 = 구글_유저_생성();
-//        userRepository.saveAll(List.of(user1, user2));
-//
-//        UserReportReqDto reportReqDto = new UserReportReqDto(BAD_MANNER, "욕해요");
-//
-//        //when
-//        userReportService.postUserReport(user1, user2.getId(), reportReqDto);
-//
-//        //then
-//        assertThat(userReportRepository.findAll()).hasSize(1);
-//    }
-//
-//}
+package com.sluv.api.domain.user.service;
+
+import com.sluv.api.user.dto.UserReportReqDto;
+import com.sluv.api.user.service.UserReportService;
+import com.sluv.domain.user.entity.User;
+import com.sluv.domain.user.service.UserDomainService;
+import com.sluv.domain.user.service.UserReportDomainService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static com.sluv.api.fixture.UserFixture.구글_유저_생성;
+import static com.sluv.api.fixture.UserFixture.카카오_유저_생성;
+import static com.sluv.domain.user.enums.UserReportReason.BAD_MANNER;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class UserReportServiceTest {
+
+    @InjectMocks
+    private UserReportService userReportService;
+
+    @Mock
+    private UserDomainService userDomainService;
+
+    @Mock
+    private UserReportDomainService userReportDomainService;
+
+    @DisplayName("사용자를 신고한다.")
+    @Test
+    void postUserReportTest() {
+        //given
+        User user = 카카오_유저_생성().toBuilder().id(1L).build();
+        User targetUser = 구글_유저_생성().toBuilder().id(2L).build();
+        when(userDomainService.findById(eq(user.getId()))).thenReturn(user);
+        when(userDomainService.findById(eq(targetUser.getId()))).thenReturn(targetUser);
+        when(userReportDomainService.findExistence(eq(user), eq(targetUser))).thenReturn(false);
+
+        UserReportReqDto reportReqDto = new UserReportReqDto(BAD_MANNER, "욕해요");
+
+        //when
+        userReportService.postUserReport(user.getId(), targetUser.getId(), reportReqDto);
+
+        //then
+        verify(userReportDomainService).saveUserReport(eq(user), eq(targetUser),
+                eq(reportReqDto.getReason()), eq(reportReqDto.getContent()));
+    }
+
+}

8f3d9b68e64725179ab4eef33a1df2860de2aaf4
KJBig <101792740+KJBig@users.noreply.github.com>
2026-01-11 20:01:51 +0900

    feat : 뉴브랜드 등록 API 테스트 코드 추가
    - sluv-api 모듈에 테스트 코드 추가

--- sluv-api/src/main/java/com/sluv/api/brand/dto/request/NewBrandPostRequest.java
+++ sluv-api/src/main/java/com/sluv/api/brand/dto/request/NewBrandPostRequest.java
@@ -1,11 +1,13 @@
 package com.sluv.api.brand.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class NewBrandPostRequest {
     @Schema(description = "새로 등록할 브랜드의 이름")
     private String newBrandName;
